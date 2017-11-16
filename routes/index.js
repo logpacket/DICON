133bf0f6798f3d60d107710450f5b070cf8df177
logpacket <199sd@naver.com>
2017-11-16 21:50:35 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	views/index.ejs

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
+var model = require('./corpmodel');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {});
+  res.render('index');
+}).post('/ajax_response',function(req,res){
+    corpname = req.body.corpname;
+    model.findOne({corpname:corpname},function(err,result){
+        if(err){
+            console.log('error'+err);
+            throw err;
+        }
+        if(result){
+            res.json(
+                {corpname:result.corpname},
+                {homepage:result.homepage},
+                {finance:result.finance},
+                {market:result.market},
+                {strockcode:result.strockcode}
+                );
+        }else {
+            res.json({
+                success:false,
+                reason:'Do not find data'
+            });
+        }
+    })
 });
+
 
 module.exports = router;
